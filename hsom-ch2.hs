b73d98e8283b78725bd736f15263d5b4523dccfc
Amadeus Dabela <amadeus.cs@icloud.com>
2020-08-24 19:52:53 -0300

    Finalizes the 2.1 exercise

--- hsom-ch2.hs
+++ hsom-ch2.hs
@@ -4,7 +4,7 @@ import Euterpea
 g251 :: Music Pitch
 g251 = let aMinor = a 3 wn :=: c 4 wn :=: e 4 wn
            dMajor = d 4 wn :=: fs 4 wn :=: a 4 wn
-           gMajor = g 4 bn :=: b 4 bn :=: d 5 bn
+           gMajor = g 3 bn :=: b 4 bn :=: d 4 bn
        in  aMinor :+: dMajor :+: gMajor
  
 
@@ -25,3 +25,29 @@ majorChord p d = note d p :=: note d (trans 4 p) :=: fifth p d
 
 fifth :: Pitch -> Dur -> Music Pitch
 fifth p d = note d (trans 7 p)
+
+{- Proof By calculation of the twoFiveOne (G, 4) wn  = g251
+twoFiveOne (G, 3) wn = let secondMinor = minorChord (trans 2 p) wn
+                           fifthMajor = majorChord (trans 7 p) wn
+                           tonicMajor = mahjorChord (G, 3) (wn * 2)
+                       in secondMinor :+: fifthMajor :+: tonicMajor
+
+After deestructuring the functions we have the following variables applied to the context =>
+
+secondMinor (A, 3) wn = minorChord (A, 3) wn = note wn a :=: note wn (trans 3 a) :=: note wn (trans 7 p)
+secondMinor (A, 3) wn = a 3 wn :=: c 3 wn :=: e 4 wn
+secondMinor (A, 3) = aMinor in g241
+
+fifthMajor (d, 4) wn = majorChord (D, 4) wn = note wn (D, 4) :=: note wn (trans 4 (D, 4)) :=: note wn (trans 7 (D, 4)
+fifthMajor (D, 4) wn = (D, 4) wn :=: (Fs, 4) wn :=: (A, 4) wn
+fifthMajor (D, 4) wn = dMajor in g251
+
+tonicMajor (G, 3) (wn * 2)= majorChord (G, 3) bn = note bn (G, 3) :=: note bn (trans 4 (G, 3)) :=: note bn (trans 7 (G, 3))
+tonicMajor (G, 3) (wn * 2) = (G, 3) bn :=: (B, 4) bn :=: (D, 4) bn
+tonicMajor (G, 3) (wn * 2) = gMajor in g251
+
+Switching the variables we conclude that
+twofiveOne (G, 3) wn = aMinor :=: dMajor :=: gMajor
+-}
+
+-- And we can define the chords by its arpeggios!
